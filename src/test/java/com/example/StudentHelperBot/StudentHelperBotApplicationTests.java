5c106e6a07c36ce989e301ee5e24fe6da7801cbb
newrav1k <kritsky.academi@gmail.com>
2024-09-14 18:41:28 +0300

    Initial commit

---
+++ src/test/java/com/example/StudentHelperBot/StudentHelperBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.StudentHelperBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudentHelperBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
