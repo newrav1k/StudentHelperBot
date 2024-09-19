57d4a4c6e1aa60fa92128170d74d1dfb72ce3dc1
newrav1k <kritsky.academi@gmail.com>
2024-09-20 02:28:10 +0300

    @Component update

--- src/main/java/com/example/StudentHelperBot/controller/type/TextController.java
+++ src/main/java/com/example/StudentHelperBot/controller/type/TextController.java
@@ -4,9 +4,11 @@ import com.example.StudentHelperBot.controller.StudentHelperBot;
 import com.example.StudentHelperBot.controller.UpdateController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
 
+@Component
 public class TextController implements UpdateController {
     private static final Logger log = LoggerFactory.getLogger(TextController.class);
 
